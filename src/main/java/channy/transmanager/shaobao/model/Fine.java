bcc688b134a45b23c4c97d801b80ad52f3e3bc66
Channy-Huang <chanfun.huang@gmail.com>
2014-11-16 23:55:24 +0800

    Make class Fine independent

--- src/main/java/channy/transmanager/shaobao/model/Fine.java
+++ src/main/java/channy/transmanager/shaobao/model/Fine.java
@@ -1,18 +1,41 @@
 package channy.transmanager.shaobao.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "fines")
-public class Fine extends Expenses {
-	public Fine() {
-		super.setType("罚单");
-	}
-	
+public class Fine extends BaseEntity {
 	/**
 	 * 
 	 */
+	@Transient
 	private static final long serialVersionUID = 3648427875204414029L;
 	
+	private String type;
+	private double amount;
+	private Date dateExpensed;
+	
+	public Date getDateExpensed() {
+		return dateExpensed;
+	}
+	public void setDateExpensed(Date dateExpensed) {
+		this.dateExpensed = dateExpensed;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 }
